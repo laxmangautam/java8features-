c0a0ded21a767c895370fc58893ec8d71bce33cb
laxmangautam <laxmangautam@gmail.com>
2019-01-26 14:44:45 +0530

    Add files via upload
    Functional interfaces can have only one interface method. It gives error.

--- Interface2.java
+++ Interface2.java
@@ -4,6 +4,10 @@ public interface Interface2 {
 
 	void method2();
 	
+	// Invalid '@FunctionalInterface' annotation; Interface2 is not a functional interface. 
+	// Functional interfaces are new concept introduced in Java 8. An interface with exactly one abstract method becomes Functional Interface
+	/* void method2(); */
+	
 	default void log(String str){
 		System.out.println("Interface1  logging::"+str);
 	}
